a29b507ed45508ccdae6bad1b7f3c6eb260fc9b5
Jonas W <hdgamer1404Jonas@gmail.com>
2024-08-19 20:01:51 +0200

    Update index.ts

--- plugins/LyricsEverywhere/src/index.ts
+++ plugins/LyricsEverywhere/src/index.ts
@@ -7,6 +7,8 @@ async function init() {
   setInterval(async () => {
     const spans = document.querySelectorAll('span[data-current="true"]');
     const textContainer = document.querySelectorAll('.wave-text-capital-demi');
+    console.log(spans)
+    console.log(textContainer)
     if (spans.length > 0 && textContainer.length > 0) {
       textContainer[0].innerHTML = spans[0].innerHTML;
     }
