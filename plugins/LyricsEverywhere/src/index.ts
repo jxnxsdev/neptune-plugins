5ac580c9d0a724b131dfd0958fdcddfa61af895d
Jonas W <hdgamer1404Jonas@gmail.com>
2024-08-19 20:13:23 +0200

    Update index.ts

--- plugins/LyricsEverywhere/src/index.ts
+++ plugins/LyricsEverywhere/src/index.ts
@@ -4,13 +4,15 @@ init();
 async function init() {
   console.log("initialising Lyrics Everywhere...");
   
-  const spans = document.querySelectorAll('span[data-current="true"]');
+  setInterval(async () => {
+    const spans = document.querySelectorAll('span[data-current="true"]');
     const textContainer = document.querySelectorAll('.wave-text-capital-demi');
     console.log(spans)
     console.log(textContainer)
     if (spans.length > 0 && textContainer.length > 0) {
       textContainer[0].innerHTML = spans[0].innerHTML;
     }
+  }, 300)
 }
 
 
