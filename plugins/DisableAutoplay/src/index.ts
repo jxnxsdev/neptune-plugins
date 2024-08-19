5d1b7bf31f2507f7cb7ac2a56c4047bec984d63c
Jonas W <hdgamer1404Jonas@gmail.com>
2024-08-19 20:41:44 +0200

    NoAutoplay

---
+++ plugins/DisableAutoplay/src/index.ts
@@ -0,0 +1,24 @@
+import { intercept, actions } from "@neptune";
+
+let autoplay: boolean = false;
+let prevSong: string | undefined;
+let isInitialised: boolean = false;
+
+function onTransition([track]: any[]) {
+  const id = (track.mediaProduct as { productId?: string })?.productId;
+  if (!id) return;
+  if (prevSong === id) return;
+  prevSong = id;
+
+  if (!autoplay) actions.playbackControls.pause();
+}
+
+const unloadTransition = intercept(
+	"playbackControls/MEDIA_PRODUCT_TRANSITION",
+	onTransition
+);
+
+
+export function onUnload() {
+  console.log("Goodbye world!");
+}
